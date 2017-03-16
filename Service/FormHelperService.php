a0ddbee89fbe7e09e0dec8731f8f145a3771c150
olexandr.balanenko <olexandr.balanenko@youshido.com>
2017-03-16 16:49:24 +0200

    add form type file

--- Service/FormHelperService.php
+++ Service/FormHelperService.php
@@ -13,6 +13,7 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilder;
@@ -96,6 +97,7 @@ class FormHelperService
             'wysiwyg' => WysiwygType::class,
             'image' => ImageType::class,
             'html' => HtmlType::class,
+            'file' => FileType::class,
         ];
         if (strpos($type, '\\') === false) {
 
